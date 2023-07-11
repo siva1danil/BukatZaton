c530e79d466eb1df2659fef189359d6bd25a6e96
siva1danil <siva1danil@ya.ru>
2023-07-11 15:48:10 +0700

    ✨ Import web data

---
+++ index.php
@@ -0,0 +1,179 @@
+﻿<!DOCTYPE html>
+<html>
+<head>
+	<meta charset="utf-8" />
+	<!--[if lt IE 9]><script src="http://html5shiv.googlecode.com/svn/trunk/html5.js"></script><![endif]-->
+	<title>Букатовский затон</title>
+    <link type="image/x-icon" rel="shortcut icon" href="favicon.ico" />
+    <meta name='wmail-verification' content='812d64a5b45e89eead982857b4a51f68' />
+    <meta name="yandex-verification" content="b844c6627ecf0e5b" />
+	<meta name="keywords" content="Турбаза Букатовский затон" />
+	<meta name="description" content="Приглашаем туристов на летнюю и зимнюю рыбалку на озера Тоджинского района, рыбалку на лодках – до водопадов Большого Енисея (в 150 км), сезонный сбор грибов, ягод, кедровых орехов. К услугам гостей – русская баня, прокат лодок. " />
+  <script src="demo.click5.ru/templates/15/bxslider/jquery.min.js"></script>
+  <script src="demo.click5.ru/templates/15/bxslider/jquery.bxslider.min.js"></script>
+  <link href="demo.click5.ru/templates/15/bxslider/jquery.bxslider.css" rel="stylesheet" />
+	<link href="demo.click5.ru/templates/15/style.css" rel="stylesheet" />
+  <script>
+  $(document).ready(function(){
+    $('.bxslider').bxSlider({
+      pager:false,
+      controls:true,
+      maxSlides:2,
+      slideWidth:400,
+      slideMargin:36,
+    });
+  });
+  </script>
+</head>
+<body>
+	<header class="header">
+    <div class="header_bg" style="background-image: url(demo.click5.ru/dogovor/371965-11/images/image_0.jpg);"><span></span></div>
+    <div class="wrapper">
+      <div class="header_info">Туристическая база</div>
+      <div class="header_logo">
+        <p>"Букатовский затон"</p>
+        <span>Верховья Большого Енисея</span>
+      </div>
+      <div class="header_service_1">
+        <p>Посуточная аренда жилья<br><br>от 500-1300 руб.</p>
+        <div style="background-image: url(demo.click5.ru/dogovor/371965-10/images/image_0.jpg);"></div>
+      </div>
+      <div class="header_service_2">
+        <p>Прокат лодок<br><br> От 1000 руб.</p>
+        <div style="background-image: url(demo.click5.ru/dogovor/371965-10/images/image_1.jpg);"></div>
+      </div>
+      <div class="header_service_3">
+        <p>Доставка и сопровождение<br><br> Групп к месту рыбалки</p>
+        <div style="background-image: url(demo.click5.ru/dogovor/371965-10/images/image_2.jpg);"></div>
+      </div>
+    </div>
+	</header><!-- .header-->
+  <div class="wrapper">
+    <div class="middle">
+
+      <div class="container">
+        <aside class="left-sidebar">
+          <nav>
+           <ul>
+              <li><a href="index.html" target="_blank">Главная</a></li>
+              <li><a href="ResponsiveImageGallery 2/galery.html">Путин рыбалка в верховьях енисея</a></li>
+              <li><a href="ResponsiveImageGallery/galery.html">Фото галерея</a></li>
+              <li><a href="links.html" target="_blank">Видео</a></li>
+              <li><a href="reviews.html" target="_blank">Статьи</a></li>
+              <li><a href="price.html" target="_blank">Прайслист</a></li>
+              <li><a href="cont.html" target="_blank">Контакты</a></li>
+             
+            </ul>
+          </nav>
+        </aside><!-- .left-sidebar -->
+        <main class="content">
+          
+             <h1 class="title">ТЕРРИТОРИЯ</h1>
+          <div class="entry">
+Расположена в 205 км от г. Кызыла, в верховьях Большого Енисея, в 30 км вверх по течению от сумона Тоора-Хем. На территории базы имеются домики для проживания а также национальные юрты.</p><p>
+         
+Приглашаем туристов на зимнюю рыбалку на озера Тоджинского района, рыбалку на лодках – до водопадов Большого Енисея (в 150 км), сезонный сбор грибов, ягод, кедровых орехов. К услугам гостей – русская баня, прокат лодок.</p><p>
+
+          </div>
+        </main><!-- .content -->
+      </div><!-- .container-->
+    </div><!-- .middle-->
+  </div>
+  <div class="sep"></div>
+  <div class="wrapper">
+    <table class="advantages">
+      <tr>
+        <td><img src="demo.click5.ru/templates/15/img/ico1.png"></td>
+        <td><img src="demo.click5.ru/templates/15/img/ico2.png"></td>
+        <td><img src="demo.click5.ru/templates/15/img/ico3.png"></td>
+        <td><img src="demo.click5.ru/templates/15/img/ico4.png"></td>
+      </tr>
+      <tr>
+        <td>
+          <p>Оперативность</p>
+          <span>Мы гарантируем быстрое и качественное обслуживание</span>
+        </td>
+        <td>
+          <p>Качество</p>
+          <span>Гарантия качества 100% на все виды услуг в нашей компании</span>
+        </td>
+        <td>
+          <p>Профессионализм</p>
+          <span>У нас работают только дипломированные специалисты!</span>
+        </td>
+        <td>
+          <p>Скидки клиентам</p>
+          <span>Мы приготовим для Вас специальное предложение!</span>
+        </td>
+      </tr>
+    </table>
+  </div>
+  <div class="sep"></div>
+  <div class="wrapper">
+    <div class="oth_title">Республика Тыва</div>
+    <div class="main_news">
+      <a href="#" target="_blank">Столица</a>
+      <p>СТОЛИЦА:г. Кызыл<br>ТЕРРИТОРИЯ:168,6 тыс. кв.км<br>НАСЕЛЕНИЕ:310 694 чел.
+      <br>ЧАСОВОЙ ПОЯС:MSK +4<br>КОД РЕГИОНА:17 </p>
+     
+    </div>
+    <div class="main_news">
+      <a href="#" target="_blank">Рельеф</a>
+      <p>РЕЛЬЕФ:горный<br>ГИДРОГРАФИЯ:самые крупные: реки – Енисей и Хемчик;<br> озера – Кара-Балык и Убсу-Нур </p>
+     
+    </div>
+    <div class="main_news">
+      <a href="#" target="_blank">Климат</a>
+      <p>КЛИМАТ:резко континентальный</p>
+      
+    </div>
+  </div>
+  <div style="clear:both"></div>
+  <div class="wrapper">
+    <div class="oth_title">Отзывы</div>
+    <ul class="bxslider">
+      <li>
+        <p class="otz_p1">Арбузова Светлана</p>
+        <p class="otz_p2">Очень довольна работой.</p>
+        <p class="otz_p3">18.02.14 Ездили отдохнуть и порыбачить,14-16 февраля на турбазу.Домики на 2 и 4 человека, кровати,чистое постельное белье,есть кухня,газ,посуда.Свет имеется тоже,стоят солнечные батареи.В домиках стоят печки -буржуйки,заводские,но зимой топить нужно постоянно и все время смотреть чтоб было тепло. </p>
+      </li>
+      <li>
+        <p class="otz_p1">Смолякова Ирина</p>
+        <p class="otz_p2">Очень благодарны</p>
+        <p class="otz_p3">В этом году мы были первыми отдыхающими.Очень понравилось,погода была хорошая,тепло,снег чистый и белый,воздух как хрусталь. </p>
+      </li>
+      <li>
+        <p class="otz_p1">Арбузова Светлана</p>
+        <p class="otz_p2">Очень довольна работой.</p>
+        <p class="otz_p3">Рыбалка правда была не очень, 8 окуньков небольших,потом правда знакомые сказали,что нужно ехать в марте,когда будет теплее и тогда и рыбалка будет отличная.В основном едут отдыхать весна,лето,осень. </p>
+      </li>
+      <li>
+        <p class="otz_p1">Смолякова Ирина</p>
+        <p class="otz_p2">Очень благодарны</p>
+        <p class="otz_p3">Дорога по зимнику хорошая,только надо быть аккуратным,крутые повороты,спуски подъемы тоже приличные,ехать так чтоб доехать туда и обратно живым и здоровым,шумахерам не место. </p>
+      </li>
+    </ul>
+  </div>
+  <div style="clear:both"></div>
+  <div class="wrapper">
+    <div class="oth_title">Схема проезда</div>
+  </div>
+<footer class="footer">
+  <div class="footer_bg">
+    <iframe src="images/map.jpg" width="100%" height="630" frameborder="0" style="border:0" allowfullscreen></iframe>
+  </div>
+  <div class="wrapper">
+    <div class="footer_cop">Все права защищены 2015</div>
+    <div class="footer_info">Туристическая база</div>
+    <div class="footer_modal">
+       <a href="cont.html" class="modal">Оставить заявку</a>
+    </div>
+    <div class="footer_phone">
+      <p>+79059204500</p>
+      <a href="mailto:olnad9@mail.ru">olnad9@mail.ru</a>
+    </div>
+  </div>
+</footer><!-- .footer -->
+
+</body>
+</html>
