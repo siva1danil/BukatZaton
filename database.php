8205465a37624349b20882b837c20938666de916
siva1danil <siva1danil@ya.ru>
2023-07-11 10:35:05 +0000

    ⚒️ Add database.php (dev)

---
+++ database.php
@@ -0,0 +1,92 @@
+<?php
+  // Load credentials
+  if (!file_exists("database_credentials.php"))
+    die("Error: Create a file <database_credentials.php> defining the following variables: <\$HOSTNAME / \$USERNAME / \$PASSWORD>");
+  include "database_credentials.php";
+  if (!isset($HOSTNAME) || !isset($USERNAME) || !isset($PASSWORD))
+    die("Error: define <\$HOSTNAME / \$USERNAME / \$PASSWORD> in your <database_credentials.php>");
+  
+  // Connect to MySQL, handle any errors
+  $conn = new mysqli($HOSTNAME, $USERNAME, $PASSWORD);
+  
+  if ($conn->connect_error) {
+    die("Error: Connection failed: " . $conn->connect_error);
+  }
+
+  // Create the database, handle any errors
+  $sql = "CREATE DATABASE IF NOT EXISTS zaton";
+  if ($conn->query($sql) != TRUE)
+    die("Error: Cannot create database: " . $conn->error);
+  
+  // Switch to our database, handle any errors
+  if ($conn->select_db("zaton") != TRUE)
+    die("Error: Cannot switch database: " . $conn->error);
+
+  // Create the reviews table, handle any errors
+  $sql = "CREATE TABLE IF NOT EXISTS reviews(
+    id INT UNSIGNED PRIMARY KEY NOT NULL AUTO_INCREMENT,
+    public BIT NOT NULL DEFAULT 0,
+    date DATETIME NOT NULL DEFAULT (CURRENT_DATE),
+    username VARCHAR(64) NOT NULL,
+    email VARCHAR(64) NOT NULL DEFAULT \"\",
+    phone INT NOT NULL DEFAULT 0,
+    review TEXT NOT NULL,
+    rating TINYINT NOT NULL)";
+  if ($conn->query($sql) != TRUE)
+    die("Error: Cannot create table: " . $conn->error);
+  
+  // Define review management functions
+  function get_reviews($show_non_public = false) {
+    global $conn;
+
+    $sql = "SELECT * FROM reviews";
+    if (!$show_non_public)
+      $sql .= " WHERE public = 1";
+    $result = $conn->query($sql);
+    if ($result->num_rows > 0)
+      return $result->fetch_all(MYSQLI_ASSOC);
+    else
+      return array();
+  }
+
+
+  function add_review($username, $email, $phone, $review, $rating) {
+    global $conn;
+
+    $sql = "INSERT INTO reviews (username, email, phone, review, rating) VALUES (?, ?, ?, ?, ?)";
+    $stmt = $conn->prepare($sql);
+    if (!$stmt)
+      die("Error: Cannot prepare statement: " . $conn->error);
+    $stmt->bind_param("ssiis", $username, $email, $phone, $review, $rating);
+    if (!$stmt->execute())
+      die("Error: Cannot execute statement: " . $stmt->error);
+    $stmt->close();
+  }
+
+  function edit_review($id, $username, $email, $phone, $review, $rating) {
+    global $conn;
+
+    $sql = "UPDATE reviews SET username = ?, email = ?, phone = ?, review = ?, rating = ? WHERE id = ?";
+    $stmt = $conn->prepare($sql);
+    if (!$stmt)
+      die("Error: Cannot prepare statement: " . $conn->error);
+    $stmt->bind_param("ssiisi", $username, $email, $phone, $review, $rating, $id);
+    if (!$stmt->execute())
+      die("Error: Cannot execute statement: " . $stmt->error);
+    $stmt->close();
+  }
+
+  function set_review_publicity($id, $value) {
+    global $conn;
+
+    $sql = "UPDATE reviews SET public = ? WHERE id = ?";
+    $stmt = $conn->prepare($sql);
+    if (!$stmt)
+      die("Error: Cannot prepare statement: " . $conn->error);
+    $stmt->bind_param("ii", $value, $id);
+    if (!$stmt->execute())
+      die("Error: Cannot execute statement: " . $stmt->error);
+    $stmt->close();
+  }
+
+?>
