aab4f27e0ae0d41af31c7f8452a2a580fd47c1c3
siva1danil <siva1danil@ya.ru>
2023-07-17 17:32:41 +0000

    ✨ set_publicity endpoint

---
+++ api/set_publicity.php
@@ -0,0 +1,29 @@
+<?php
+  session_start();
+  include (__DIR__ . "/../lib/database.php");
+
+  if($_SESSION["logged_in"] != TRUE)
+    printf(json_encode([ "error" => "Вы не авторизованы" ]));
+  else {
+    if (!is_null(init())) {
+        printf(json_encode([ "error" => "Ошибка базы данных, обратитесь к администратору" ]));
+    } else {
+        $json = file_get_contents('php://input');
+        $data = json_decode($json, true);
+        
+        if(is_null($data)) {
+            printf(json_encode([ "error" => "Неверный формат запроса" ]));
+        } else {
+            $validId = strlen(preg_replace("/\D/", "", $data["id"])) > 0;
+            $validPublicity = $data["public"] == TRUE || $data["public"] == FALSE;
+
+            if ($validId && $validPublicity) {
+                set_review_publicity(preg_replace("/\D/", "", $data["id"]), $data["public"]);
+                printf(json_encode([ "error" => NULL ]));
+            } else {
+                printf(json_encode([ "error" => "Параметры указаны неверно" ]));
+            }
+        }
+    }
+  }
+?>
