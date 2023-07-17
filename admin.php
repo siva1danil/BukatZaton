af61dcba637c12b5698a3a31c9653aa9cf4dc9fb
siva1danil <siva1danil@ya.ru>
2023-07-17 18:24:23 +0000

    ✨ Admin panel

--- admin.php
+++ admin.php
@@ -5,6 +5,7 @@
   <title>Администратор - Букатовский затон</title>
   <meta charset="utf8">
   <link type="image/x-icon" rel="shortcut icon" href="favicon.ico" />
+  <link href="/res/style.css" rel="stylesheet" />
   <link rel="stylesheet" href="/res/style-admin.css" />
   <script src="/res/script-admin.js"></script>
 </head>
@@ -13,9 +14,74 @@
 
   <div class="wrap">
     <div class="content">
-      content
+      <div class="header">
+        <span class="header-title">Управление отзывами</span>
+        <a id="button-logout" class="header-logout" href="#">Выход</a>
+      </div>
+      <span class="reviews-title">Опубликованные отзывы</span>
+      <div id="reviews-public" class="reviews"></div>
+      <span class="reviews-empty">Пусто</span>
+      <span class="reviews-title">Ожидают модерации</span>
+      <div id="reviews-private" class="reviews"></div>
+      <span class="reviews-empty">Пусто</span>
     </div>
   </div>
+
+  <div id="edit-background" class="edit-background">
+    <form id="edit-form" class="edit" onsubmit="return false;">
+      <table class="reviews-add-form-table">
+        <tr>
+            <td class="reviews-add-form-inputname">Автор:</td>
+            <td><input class="reviews-add-form-smallinput" type="text" name="username"></td>
+        </tr>
+        <tr>
+            <td class="reviews-add-form-inputname">E-Mail (необязательно):</td>
+            <td><input class="reviews-add-form-smallinput" type="email" name="email"></td>
+        </tr>
+        <tr>
+            <td class="reviews-add-form-inputname">Телефон (необязательно):</td>
+            <td><input class="reviews-add-form-smallinput" type="phone" name="phone"></td>
+        </tr>
+        <tr>
+            <td class="reviews-add-form-inputname">Оценка:</td>
+            <td>
+              <div class="reviews-add-form-stars">
+                <input type="radio" id="reviews-add-form-star5" name="rating" value="5" checked /><label for="reviews-add-form-star5">★</label>
+                <input type="radio" id="reviews-add-form-star4" name="rating" value="4" /><label for="reviews-add-form-star4">★</label>
+                <input type="radio" id="reviews-add-form-star3" name="rating" value="3" /><label for="reviews-add-form-star3">★</label>
+                <input type="radio" id="reviews-add-form-star2" name="rating" value="2" /><label for="reviews-add-form-star2">★</label>
+                <input type="radio" id="reviews-add-form-star1" name="rating" value="1" /><label for="reviews-add-form-star1">★</label>
+              </div>
+            </td>
+        </tr>
+      </table>
+
+      <textarea class="reviews-add-form-biginput" placeholder="Текст отзыва" rows="5" name="review"></textarea>
+
+      <div class="reviews-add-form-bottombar">
+        <button id="edit-cancel" class="reviews-add-form-button">Отмена</input>
+        <button id="edit-save" class="reviews-add-form-button">Сохранить</input>
+      </div>
+    </form>
+  </div>
+
+  <template id="template-review">
+    <div class="review">
+      <div class="review-header">
+        <span class="review-stars"><span>★</span><span>★</span><span>★</span><span>★</span><span>★</span></span>
+        <span class="review-name">Имя</span>
+      </div>
+      <div class="review-contacts">
+        <span class="review-phone">телефон не указан</span> / <span class="review-email">почта не указана</span>
+      </div>
+      <span class="review-text">Текст отзыва</span>
+      <div class="review-actions">
+        <a class="review-action review-change-publicity" href="#"></a>
+        <a class="review-action review-edit" href="#">Изменить</a>
+        <a class="review-action review-delete" href="#">Удалить</a>
+      </div>
+    </div>
+  </template>
 
 <?php } else { ?>
 
--- res/script-admin.js
+++ res/script-admin.js
@@ -9,6 +9,7 @@ window.addEventListener('load', () => {
         console.log(UI);
 
         UI.button.addEventListener('click', async () => {
+            if(UI.button.disabled) return;
             UI.button.disabled = true;
 
             let data = {
@@ -30,5 +31,165 @@ window.addEventListener('load', () => {
 
             UI.button.disabled = false;
         });
+    } else {
+        const UI = {
+            reviewTemplate: document.getElementById('template-review'),
+            logout: document.getElementById('button-logout'),
+            reviewsPublic: document.getElementById('reviews-public'),
+            reviewsPrivate: document.getElementById('reviews-private'),
+            editBackground: document.getElementById('edit-background'),
+            editForm: document.getElementById('edit-form'),
+            editCancel: document.getElementById('edit-cancel'),
+            editSave: document.getElementById('edit-save'),
+        };
+        const Actions = {
+            setReviews: reviews => {
+                UI.reviewsPublic.innerText = '';
+                UI.reviewsPrivate.innerText = '';
+
+                for(let review of reviews) {
+                    let element = UI.reviewTemplate.content.children[0].cloneNode(true);
+
+                    for(let i = review.rating; i < 5; i++)
+                        element.getElementsByClassName('review-stars')[0].children[i].style.color = 'transparent';
+                    element.getElementsByClassName('review-name')[0].innerText = review.username;
+                    if(review.phone != 0)
+                        element.getElementsByClassName('review-phone')[0].innerText = review.phone;
+                    if(review.email != "")
+                        element.getElementsByClassName('review-email')[0].innerText = review.email;
+                    element.getElementsByClassName('review-text')[0].innerText = review.review;
+
+                    element.getElementsByClassName('review-change-publicity')[0].addEventListener('click', async event => {
+                        if(event.target.disabled) return;
+                        event.target.disabled = true;
+                        element.style.opacity = 0.5;
+
+                        await API.setPublicity(review.id, !review.public)
+                            .then(() => Actions.reload())
+                            .catch(error => alert(error));
+
+                        event.target.disabled = false;
+                        element.style.opacity = 1.0;
+                    });
+                    element.getElementsByClassName('review-edit')[0].addEventListener('click', async event => {
+                        if(event.target.disabled) return;
+                        event.target.disabled = true;
+                        element.style.opacity = 0.5;
+
+                        while(true) {
+                            review = await Actions.edit(review);
+                            if(review == null) break;
+
+                            try {
+                                await API.editReview(review).then(() => Actions.reload());
+                                break;
+                            } catch(error) {
+                                alert(error);
+                            };
+                        }
+
+                        event.target.disabled = false;
+                        element.style.opacity = 1.0;
+                    });
+                    element.getElementsByClassName('review-delete')[0].addEventListener('click', async event => {
+                        if(event.target.disabled) return;
+                        event.target.disabled = true;
+                        element.style.opacity = 0.5;
+
+                        await API.deleteReview(review.id)
+                            .then(() => Actions.reload())
+                            .catch(error => alert(error));
+
+                        event.target.disabled = false;
+                        element.style.opacity = 1.0;
+                    });
+
+                    if(review.public)
+                        UI.reviewsPublic.appendChild(element);
+                    else
+                        UI.reviewsPrivate.appendChild(element);
+                }
+            },
+            reload: async () => {
+                await API.getReviews().then(data => Actions.setReviews(data));
+            },
+            edit: async review => new Promise((resolve, reject) => {
+                UI.editBackground.style.display = 'flex';
+                const cancel = UI.editCancel.cloneNode(true);
+                UI.editCancel.replaceWith(cancel);
+                UI.editCancel = cancel;
+                const save = UI.editSave.cloneNode(true);
+                UI.editSave.replaceWith(save);
+                UI.editSave = save;
+                UI.editForm.username.value = review.username;
+                UI.editForm.phone.value = review.phone == 0 ? '' : review.phone;
+                UI.editForm.email.value = review.email;
+                UI.editForm.rating.value = review.rating;
+                UI.editForm.review.value = review.review;
+                UI.editCancel.addEventListener('click', () => {
+                    UI.editBackground.style.display = 'none';
+                    resolve(null);
+                });
+                UI.editSave.addEventListener('click', () => {
+                    UI.editBackground.style.display = 'none';
+                    resolve({ id: review.id, name: review.public, date: review.date,
+                        username: UI.editForm.username.value, email: UI.editForm.email.value,
+                        phone: UI.editForm.phone.value, rating: UI.editForm.rating.value,
+                        review: UI.editForm.review.value });
+                });
+            })
+        };
+        const API = {
+            logout: async () => {
+                let data = await fetch('/api/logout.php')
+                    .then(res => res.json());
+                if(data.error) throw new Error(data.error);
+            },
+            getReviews: async () => {
+                let data = [];
+                for(let i = 1; true; i++) {
+                    let page = await fetch(`/api/get_reviews.php?page=${i}`)
+                        .then(res => res.json());
+                    if(page.error) throw new Error(page.error);
+                    else if(page.data.length != 0) data.push(...page.data);
+                    else break;
+                }
+                return data;
+            },
+            deleteReview: async id => {
+                let data = await fetch('/api/delete_review.php', {
+                    method: 'POST',
+                    body: JSON.stringify({ id })
+                }).then(res => res.json());
+                if(data.error) throw new Error(data.error);
+            },
+            setPublicity: async (id, public) => {
+                let data = await fetch('/api/set_publicity.php', {
+                    method: 'POST',
+                    body: JSON.stringify({ id, public })
+                }).then(res => res.json());
+                if(data.error) throw new Error(data.error);
+            },
+            editReview: async review => {
+                let data = await fetch('/api/edit_review.php', {
+                    method: 'POST',
+                    body: JSON.stringify(review)
+                }).then(res => res.json());
+                if(data.error) throw new Error(data.error);
+            }
+        };
+
+        UI.logout.addEventListener('click', async () => {
+            if(UI.logout.disabled) return;
+            UI.logout.disabled = true;
+
+            await API.logout()
+                .then(() => location.reload())
+                .catch(error => alert(error));
+            
+            UI.logout.disabled = false;
+        });
+
+        Actions.reload();
     }
 });
