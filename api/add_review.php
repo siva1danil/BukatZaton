8ecd980505a81fa33950c05344cfe436f27e9e47
siva1danil <siva1danil@ya.ru>
2023-07-17 15:34:12 +0000

    🐛 add_review: fix phone number normalization

--- api/add_review.php
+++ api/add_review.php
@@ -12,14 +12,14 @@
     } else {
       $validUsername = strlen($data["username"]) <= 64 && preg_match('/^(\s+)?$/', $data["username"]) == FALSE;
       $validEmail = strlen($data["email"]) <= 64 && (preg_match('/^(\s+)?$/', $data["email"]) != FALSE || preg_match('/^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/', $data["email"]) != FALSE);
-      $validPhone = preg_match('/^(\s+)?$/', $data["phone"]) != FALSE || count(preg_replace("\D", "", $data["phone"])) == 11;
+      $validPhone = preg_match('/^(\s+)?$/', $data["phone"]) != FALSE || strlen(preg_replace('/\D/', "", $data["phone"])) == 11;
       $validRating = $data["rating"] >= 1 && $data["rating"] <= 5;
       $validReview = preg_match('/^(\s+)?$/', $data["review"]) == FALSE;
 
       if ($validUsername && $validEmail && $validPhone && $validRating && $validReview) {
         add_review($data["username"],
           preg_match('/^(\s+)?$/', $data["email"]) != FALSE ? "" : $data["email"],
-          preg_match('/^(\s+)?$/', $data["phone"]) != FALSE ? "0" : preg_replace("\D", "", $data["phone"]),
+          preg_match('/^(\s+)?$/', $data["phone"]) != FALSE ? "0" : preg_replace('/\D/', "", $data["phone"]),
           $data["review"],
           $data["rating"]);
         printf(json_encode([ "error" => NULL ]));
